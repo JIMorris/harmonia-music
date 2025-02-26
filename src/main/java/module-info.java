7b2c91cfa11b411c1cb503f94e53927f50200187
Isaac Morris <xxjamesblond007xx@gmail.com>
2025-02-26 18:27:36 -0500

    Added base code

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,11 @@
 module com.harmoniamusic {
     requires javafx.controls;
     requires javafx.fxml;
+    requires json.simple;
 
     opens com.harmoniamusic to javafx.fxml;
     exports com.harmoniamusic;
+
+    opens com.model to javafx.fxml;
+    exports com.model;
 }
