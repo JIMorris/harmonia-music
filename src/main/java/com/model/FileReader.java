da02df782845b9fec170c49d79e65221ad3ff476
Isaac Morris <xxjamesblond007xx@gmail.com>
2025-03-02 01:10:50 -0500

    Added FileReader Stub (close #42)

---
+++ src/main/java/com/model/FileReader.java
@@ -0,0 +1,32 @@
+package com.model;
+
+public class FileReader {
+    private static FileReader instance;
+    private UserList userList;
+    private SongList songList;
+    private InstrumentList instrumentList;
+
+    private FileReader(){
+        //TODO
+    }
+
+    public static FileWriter getInstance(){
+        //TODO
+        return null;
+    }
+
+    public boolean saveUsers(){
+        //TODO
+        return false;
+    }
+
+    public boolean saveSongs(){
+        //TODO
+        return false;
+    }
+
+    public boolean saveInstruments(){
+        //TODO
+        return false;
+    }
+}
