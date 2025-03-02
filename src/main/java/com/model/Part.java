aed775057f9090c427210be2b590eeda51385d34
Isaac Morris <xxjamesblond007xx@gmail.com>
2025-03-02 01:01:57 -0500

    Added Part stub (close #14)

---
+++ src/main/java/com/model/Part.java
@@ -0,0 +1,19 @@
+package com.model;
+
+public class Part {
+    private Instrument instrument;
+    private ArrayList<Note> music;
+    private String test;
+
+    public Part(Instrument instrument){
+        //TODO
+    }
+
+    public boolean insertNote(Note note, int position){
+        //TODO
+    }
+
+    public boolean removeNote(int position){
+        //TODO
+    }
+}
