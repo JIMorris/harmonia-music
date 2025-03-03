dcc8ca18db8ddadc97b6634fc0d06f77a269071b
kcrase <kcrase515@gmail.com>
2025-03-03 07:58:39 -0500

    Created files: instrument and instrumentlist; Added stubs for both files

---
+++ src/main/java/com/model/InstrumentList.java
@@ -0,0 +1,30 @@
+package com.model;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class InstrumentList {
+    private static InstrumentList instance;
+    private ArrayList<Instrument> instruments;
+
+    private InstrumentList() {
+
+    }
+
+    public InstrumentList getInstance() {
+        return null;
+    }
+
+    public addInstrument(Instrument instrument) {
+        return null;
+    }
+
+    public removeInstrument(Instrument instrument) {
+        return null;
+    }
+
+    public save() {
+        return null;
+    }
+
+}
---
+++ src/main/java/com/model/Instruments.java
@@ -0,0 +1,14 @@
+package com.model;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class Song {
+    private UUID instrumentID;
+    private String name;
+    private String imageFile;
+
+    public Song(UUID instrumentID, String name, User imageFile) {
+        
+    }
+}
