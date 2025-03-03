82d87b8828d7de4a1e8db13fe9961f9261dcc5e4
kcrase <kcrase515@gmail.com>
2025-03-03 08:14:31 -0500

    Added documentation for files: Instrument and InstrumentList

--- src/main/java/com/model/Instrument.java
+++ src/main/java/com/model/Instrument.java
@@ -3,11 +3,24 @@ package com.model;
 import java.util.ArrayList;
 import java.util.UUID;
 
+/**
+ * class which creates an Instrument object which has an instrumentID (UUID), 
+ * name(String), and an image file(String)
+ * 
+ * @author kcrase
+ */
 public class Instrument {
     private UUID instrumentID;
     private String name;
     private String imageFile;
 
+    /**
+     * constructor that creates a new instrument
+     * 
+     * @param instrumentID takes in a UUID for the instrumentID of the new instrument
+     * @param name takes in a string for the name of the new instrument
+     * @param imageFile takes in a string for the image file of the new instrument
+     */
     public Instrument(UUID instrumentID, String name, User imageFile) {
         
     }
--- src/main/java/com/model/InstrumentList.java
+++ src/main/java/com/model/InstrumentList.java
@@ -3,26 +3,48 @@ package com.model;
 import java.util.ArrayList;
 import java.util.UUID;
 
+/**
+ * class that creates an instance which has an arraylist of instruments
+ * 
+ * @author kcrase
+ */
 public class InstrumentList {
     private static InstrumentList instance;
     private ArrayList<Instrument> instruments;
 
+    /**
+     * private constructor that creates a single instance of the InstrumentList 
+     * object
+     */
     private InstrumentList() {
 
     }
 
+    /**
+     * public method which calls the private InstrumentList constructor, 
+     * returns the instance of InstrumentList
+     */
     public InstrumentList getInstance() {
         return null;
     }
 
+    /**
+     * public method which adds a new instrument to the arraylist
+     */
     public addInstrument(Instrument instrument) {
         return null;
     }
-
+    
+    /**
+     * public method which removes an instrument from the arraylist
+     */
     public removeInstrument(Instrument instrument) {
         return null;
     }
 
+    /**
+     * public method which saves/updates the arraylist of instruments
+     */
     public save() {
         return null;
     }
