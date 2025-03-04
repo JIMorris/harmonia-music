40dc06927986a1797b9c3ddeb617a2513cdb0eb4
Isaac Morris <xxjamesblond007xx@gmail.com>
2025-03-04 14:10:29 -0500

    Filled User and Instrument constants (#43)

--- src/main/java/com/model/DataConstants.java
+++ src/main/java/com/model/DataConstants.java
@@ -1,17 +1,17 @@
 package com.model;
 
 public class DataConstants {
-    public static final String USER_FILE_NAME = "";
-    public static final String USER_ID = "";
-    public static final String USER_USERNAME = "";
-    public static final String USER_PASSWORD = "";
-    public static final String USER_FIRST_NAME = "";
-    public static final String USER_LAST_NAME = "";
-    public static final String USER_FAV_SONGS = "";
-    public static final String USER_FAV_AUTHS = "";
+    public static final String USER_FILE_NAME = "json/user.json";
+    public static final String USER_ID = "uuid";
+    public static final String USER_USERNAME = "username";
+    public static final String USER_PASSWORD = "password";
+    public static final String USER_FIRST_NAME = "firstName";
+    public static final String USER_LAST_NAME = "lastName";
+    public static final String USER_FAV_SONGS = "favSongs";
+    public static final String USER_FAV_AUTHS = "favAuthors";
 
-    public static final String INSTRUMENT_FILE_NAME = "";
-    public static final String INSTRUMENT_ID = "";
-    public static final String INSTRUMENT_NAME = "";
-    public static final String INSTRUMENT_IMAGE_FILE = "";
+    public static final String INSTRUMENT_FILE_NAME = "json/instrument.json";
+    public static final String INSTRUMENT_ID = "uuid";
+    public static final String INSTRUMENT_NAME = "name";
+    public static final String INSTRUMENT_IMAGE_FILE = "imageFile";
 }
