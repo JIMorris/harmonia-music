cb71eedcebd7a198277dcdd766c346953cac0c3c
scartisp <simion.cartis1@gmail.com>
2025-03-06 00:30:36 -0500

    created the Pitch stub (resolved #20)

---
+++ src/main/java/com/model/Pitch.java
@@ -0,0 +1,30 @@
+package com.model;
+
+public enum Pitch {
+    REST(""),
+    A_FLAT(""),
+    A(""),
+    A_SHARP(""),
+    B_FLAT(""),
+    B(""),
+    C(""),
+    C_SHARP(""),
+    D_FLAT(""),
+    D(""),
+    D_SHARP(""),
+    E_FLAT(""),
+    E(""),
+    F(""),
+    F_SHARP(""),
+    G_FLAT(""),
+    G(""),
+    G_SHARP("");
+
+    public final String label;
+
+    private Pitch(String label) {
+        this.label = label;
+    }
+    
+
+}
