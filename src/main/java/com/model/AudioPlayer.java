9752abc20249e36c0cf6e455f3574d5225807892
ekeisler321 <edirisk.21@gmail.com>
2025-03-06 13:25:29 -0500

    Pushed Audio class stub #10

---
+++ src/main/java/com/model/AudioPlayer.java
@@ -0,0 +1,41 @@
+package com.model;
+
+public class AudioPlayer {
+    private Song song;
+    private int currentMeasure;
+    private Instrument currentInstrument;
+    private double playbackSpeed;
+    private boolean metronome;
+
+    public AudioPlayer (Song song){
+        this.song = song;
+    }
+
+    public void setPlaybackSpeed(double speed) {
+        
+    }
+
+    public void selectPart(Instrument instrument) {
+
+    }
+
+    public void play() {
+
+    }
+
+    public void pause() {
+
+    }
+
+    public void stop() {
+
+    }
+
+    public void back() {
+
+    }
+
+    public void forward() {
+        
+    }
+}
