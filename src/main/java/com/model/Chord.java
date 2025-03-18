df07c58b914c54e83f5ec455df7e6d01497d5fb1
scartisp <simion.cartis1@gmail.com>
2025-03-18 13:25:37 -0400

    wrote the logic for the Chord enum (resolved #78)

--- src/main/java/com/model/Chord.java
+++ src/main/java/com/model/Chord.java
@@ -1,36 +1,5 @@
 package com.model;
 
-import java.util.ArrayList;
-
-// Change to Enum
-// public class Chord {
-//     private String name;
-//     private String abreviation; //MISSPELLED IN THE UML
-//     private  ArrayList<Pitch> notes;
-
-//     public Chord(String name, String abbreviation, ArrayList<Pitch> notes) {
-
-//     }
-
-//     public Chord(Pitch root, String type) {
-
-//     }
-
-//     public Pitch getRoot() {
-//         return null;
-//     }
-
-//     /**
-//      * TODO
-//      * @param chord
-//      * @return
-//      */
-//     public static Chord fromString(String chord){
-//         //TODO
-//         return null;
-//     }
-// }
-
 public enum Chord {
     A_FLAT_MAJ("Abmaj"),
     A_FLAT_MIN("Abmin"),
