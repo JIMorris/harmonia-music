ff0ddac36d8446ff27265521e6d868fd0cea8872
dacrafter1234567 <dacrafter247@gmail.com>
2025-03-19 01:13:19 -0400

    Note JavaDoc and Methods

--- src/main/java/com/model/Note.java
+++ src/main/java/com/model/Note.java
@@ -1,16 +1,36 @@
 package com.model;
 
+/**
+ * Represents a musical note with a duration, pitch, and octave
+ * @author 
+ */
 public class Note {
     public static int QUARTER_LENGTH;
     private int duration;
     private Pitch pitch;
     private int octave;
 
-    public Note(int duration, Pitch pitch, int octave) {
 
+    /**
+     * Constructs a note object with a duration, pitch, and octave
+     * @param duration
+     * @param pitch
+     * @param octave
+     */
+    public Note(int duration, Pitch pitch, int octave) {
+        this.duration = duration;
+        this.pitch = pitch;
+        this.octave = octave;
     }
 
+    /**
+     * Constructs a note with the given duration and note name.
+     * @param duration
+     * @param noteName
+     */
     public Note (int duration, String noteName) {
-        
+        this.duration = duration;
+        this.pitch = Pitch.fromString(noteName);
+        this.octave = 4; //default octave, can be modified later
     }
 }
