99e1bb1e5112364a1b49aa16e5c284e30fe82c99
scartisp <simion.cartis1@gmail.com>
2025-03-25 13:23:31 -0400

    documented User and UserList (resolved #111 and resolved #112)

--- src/main/java/com/model/User.java
+++ src/main/java/com/model/User.java
@@ -5,6 +5,8 @@ import java.util.UUID;
 
 /**
  * Represents a User with personal details and favorite songs and authors
+ * 
+ * @author Simion Cartis
  */
 public class User {
     private String username;
@@ -19,12 +21,12 @@ public class User {
     /**
      * Constructor for a new User
      * 
-     * @param username Username of user
-     * @param password Password of user
-     * @param firstName First Name of user
-     * @param lastName Last Name of user
+     * @param username  Username of user as a String
+     * @param password  Password of user as a String
+     * @param firstName First Name of user as a String
+     * @param lastName  Last Name of user as a String
      */
-    public User(String username, String password, String firstName, String lastName){
+    public User(String username, String password, String firstName, String lastName) {
         this.username = username;
         this.password = password;
         this.firstName = firstName;
@@ -35,13 +37,13 @@ public class User {
     }
 
     /**
-     * Constructor for User class with parameters
+     * Constructor for User class which includes a userID parameter (used for loading into json)
      * 
-     * @param username the username provided by the user
-     * @param password the password provided by the user
-     * @param firstName the first name provided of the user
-     * @param lastName the last name provided by the user
-     * @param userID a unique ID code used to indentify each individual user
+     * @param username  The username provided by the user
+     * @param password  The password provided by the user
+     * @param firstName The first name provided of the user
+     * @param lastName  The last name provided by the user
+     * @param userID    A unique ID code used to indentify each individual user
      */
     public User(String username, String password, String firstName, String lastName, UUID userID) {
         this.username = username;
@@ -56,16 +58,16 @@ public class User {
     /**
      * Retrieves the username of the user
      *
-     * @return the username 
+     * @return The username
      */
     public String getUsername() {
         return this.username;
     }
 
     /**
-     * Retrieves the password of the user 
+     * Retrieves the password of the user
      * 
-     * @return the password 
+     * @return The password
      */
     public String getPassword() {
         return this.password;
@@ -74,24 +76,44 @@ public class User {
     /**
      * Retrieves the unique user ID for the user
      * 
-     * @return the user's ID
+     * @return The user's ID (which is a UUID)
      */
     public UUID getUserID() {
         return this.userID;
     }
 
+    /**
+     * Retrieves a users favorite songs
+     * 
+     * @return The user's favorite songs (which is an ArrayList of type Song)
+     */
     public ArrayList<Song> getFavSongs() {
         return this.favSongs;
     }
-    
+
+    /**
+     * Retrieves a users favorite authors
+     * 
+     * @return The user's favorite authors (which is an ArrayList of type User)
+     */
     public ArrayList<User> getFavAuthors() {
         return this.favAuthors;
     }
 
+    /**
+     * Retrieves the user's first name
+     * 
+     * @return The user's first name (of type String)
+     */
     public String getFirstName() {
         return this.firstName;
     }
 
+    /**
+     * Retrieves the user's last name
+     * 
+     * @return The user's last name (of type String)
+     */
     public String getLastName() {
         return this.lastName;
     }
@@ -101,25 +123,27 @@ public class User {
      * 
      * @param favAuthors What to set favAuthors to
      */
-    public void setFavoriteAuthors(ArrayList<User> favAuthors){
+    public void setFavoriteAuthors(ArrayList<User> favAuthors) {
         this.favAuthors = favAuthors;
     }
 
     /**
      * Checks if the userID matches the user's unique ID
      * 
-     * @param userID the UUID that needs to be checked
-     * @return true if the ID's match, otherwise it is false
+     * @param userID The UUID that needs to be checked
+     * @return True if the ID's match, otherwise it is false
      */
     public boolean idMatch(UUID userID) {
         return this.userID.equals(userID);
     }
 
     /**
-     * Checks if the password provided by the user matches the user's provided username
+     * Checks if the password provided by the user matches the user's provided
+     * username
      * 
-     * @param password the password that needs to be checked
-     * @return true if the provided password matches the user's username, otherwise it is false
+     * @param password The password that needs to be checked
+     * @return True if the provided password matches the user's username, otherwise
+     *         it is false
      */
     public boolean passwordMatch(String password) {
         return this.password.equals(password);
@@ -128,31 +152,35 @@ public class User {
     /**
      * Checks if the password provided by the user matches the user's username
      * 
-     * @param username the username that needs to be checked
-     * @return true if the usernames are a match, otherwise it is false
+     * @param username The username that needs to be checked
+     * @return True if the usernames are a match, otherwise it is false
      */
     public boolean usernameMatch(String username) {
         return this.username.equals(username);
     }
 
     /**
-     * Adds a song to the user's list of their favorite songs given it is not already in the favorite's list 
+     * Adds or removes a Song from the favorite songs ArrayList depending on wether
+     * or not they are already in the list
      * 
-     * @param song the song the user wishes to add
+     * @param song The song the user wishes to add
      */
     public void toggleFavoriteSong(Song song) {
-        if(this.favSongs.contains(song))
+        if (this.favSongs.contains(song))
             favSongs.remove(song);
         else
             favSongs.add(song);
     }
 
     /**
-     * TODO
-     * @param author
+     * Adds or removes a User from the favorite author ArrayList depending on wether
+     * or not they are already the list
+     * 
+     * @param author Is of type User and is the user that will be added/removed from
+     *               the ArrayList
      */
-    public void toggleFavoriteAuthor(User author){
-        if(this.favAuthors.contains(author))
+    public void toggleFavoriteAuthor(User author) {
+        if (this.favAuthors.contains(author))
             favAuthors.remove(author);
         else
             favAuthors.add(author);
