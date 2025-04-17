ca485b0786eab4302ca4457cbeaf597ef649e4f0
scartisp <simion.cartis1@gmail.com>
2025-04-17 17:07:29 -0400

    worked on songListcontroller (#219)

--- src/main/java/com/harmoniamusic/SongListController.java
+++ src/main/java/com/harmoniamusic/SongListController.java
@@ -5,27 +5,40 @@ import java.util.ArrayList;
 import com.model.MusicFacade;
 import com.model.SongList;
 import com.model.UserList;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.VBox;
+
 import com.model.Song;
 
 public class SongListController {
     private static MusicFacade musicFacade = MusicFacade.getInstance();
 
     public static void loadsongs(int type) {
-        ArrayList<Song> songs;
+        // ArrayList<Song> songs;
+        ListView<Song> listView = null;
+        ObservableList<Song> songs;
         switch (type) {
             case 1:
-                songs = musicFacade.openMySongs();
+                songs = FXCollections.observableArrayList(musicFacade.openMySongs());
+                listView = new ListView<>(songs);
                 break;
             case 2:
-                songs = musicFacade.openFavorites();
+                songs = FXCollections.observableArrayList(musicFacade.openFavorites());
+                listView = new ListView<>(songs);
                 break;
             case 3:
-                songs = musicFacade.openPublicSongs();
+                songs = FXCollections.observableArrayList(musicFacade.openPublicSongs());
+                listView = new ListView<>(songs);
                 break;
 
             default:
                 break;
         }
-
+         VBox root = new VBox(listView);
+        Scene scene = new Scene(root, 300, 200);
     }
 }
