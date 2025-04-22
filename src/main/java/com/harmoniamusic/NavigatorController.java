892629b1327d81b3ef016cd92d95cc3a83b36b87
Isaac Morris <xxjamesblond007xx@gmail.com>
2025-04-21 20:09:34 -0400

    Added FXML tag (#218)

--- src/main/java/com/harmoniamusic/NavigatorController.java
+++ src/main/java/com/harmoniamusic/NavigatorController.java
@@ -4,26 +4,32 @@ import java.io.IOException;
 
 import com.model.MusicFacade;
 
+import javafx.fxml.FXML;
+
 public class NavigatorController {
     private MusicFacade musicFacade = MusicFacade.getInstance();
 
     //methods for moving from home screen
 
+    @FXML
     private void goToFavoriteAuthors() throws IOException {
         App.setBar("libraryBar");
         App.setData("favoriteAuthorsData");
     }
 
+    @FXML
     private void goToMySongs() throws IOException {
         App.setBar("libraryBar");
         App.setData("mySongsData");
     }
 
+    @FXML
     private void gotoFavoritesongs() throws IOException {
         App.setBar("libraryBar");
         App.setData("favoriteSongsData");
     }
 
+    @FXML
     private void goToPublicSongs() throws IOException { 
         App.setBar("libraryBar");
         App.setData("publicSongsData");
@@ -31,11 +37,13 @@ public class NavigatorController {
 
     //methods for top bar
 
+    @FXML
     private void goToHome() throws IOException {
         App.setBar("homeBar");
         App.setData("homeData");
     }
 
+    @FXML
     private void goToSettings() throws IOException {
         App.setBar("settingsBar");
         App.setData("settingsData");
