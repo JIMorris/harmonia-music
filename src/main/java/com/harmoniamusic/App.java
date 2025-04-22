5996b1720b6d4752728ca5f9e28a9ac4c01c6098
scartisp <simion.cartis1@gmail.com>
2025-04-22 17:42:39 -0400

    fixed file pathing for loginTemplate

--- src/main/java/com/harmoniamusic/App.java
+++ src/main/java/com/harmoniamusic/App.java
@@ -25,7 +25,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("loginTemplate"), 640, 480);
+        scene = new Scene(loadFXML("templates/loginTemplate"), 640, 480);
         stage.setScene(scene);
         stage.show();
        // setData("loginData");
