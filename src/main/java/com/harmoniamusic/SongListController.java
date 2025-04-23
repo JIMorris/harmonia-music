621107624fd01ea78e66e405e73038afd533e1f5
scartisp <simion.cartis1@gmail.com>
2025-04-22 21:00:52 -0400

    added functionality to publicSong and favoriteSong lists (#224)

--- src/main/java/com/harmoniamusic/SongListController.java
+++ src/main/java/com/harmoniamusic/SongListController.java
@@ -21,32 +21,6 @@ import javafx.stage.Stage;
 public class SongListController extends Application {
     private static MusicFacade musicFacade = MusicFacade.getInstance();
 
-    // @FXML
-    // private static ListView<Song> listView;
-    // public static void loadsongs(int type) {
-    // // ArrayList<Song> songs;
-    // //ListView<Song> listView = null;
-    // ObservableList<Song> songs;
-    // switch (type) {
-    // case 1:
-    // songs = FXCollections.observableArrayList(musicFacade.openMySongs());
-    // listView = new ListView<>(songs);
-    // break;
-    // case 2:
-    // songs = FXCollections.observableArrayList(musicFacade.openFavorites());
-    // listView = new ListView<>(songs);
-    // break;
-    // case 3:
-    // songs = FXCollections.observableArrayList(musicFacade.openPublicSongs());
-    // listView = new ListView<>(songs);
-    // break;
-
-    // default:
-    // break;
-    // }
-    // // VBox root = new VBox(listView);
-    // // Scene scene = new Scene(root, 300, 200);
-    // }
     @FXML
     private static Accordion accordion;
 
@@ -60,9 +34,9 @@ public class SongListController extends Application {
                     VBox pane1Content = new VBox(10);
                     Button playButton = new Button("play song");
                     playButton.setOnMouseClicked(e -> {
-                        //TODO
+                        // TODO
                     });
-                    Button addFav =  new Button("add to favorites");
+                    Button addFav = new Button("add to favorites");
                     addFav.setOnMouseClicked(e -> {
                         musicFacade.toggleFavorite(song);
                     });
@@ -79,24 +53,18 @@ public class SongListController extends Application {
                         song.setDescription(setDescription.getText());
                         setDescription.setText(song.getDescription());
                     });
-                    // Label Genre = new Label("Genres");
-                    // TextField setGenre = new TextField(song.viewGenres());
-                    // setDescription.setOnAction(e -> {
-                    //     song.set(setDescription.getText());
-                    //     setDescription.setText(song.getDescription());
-                    // });
                     Button editSong = new Button("edit song");
                     editSong.setOnMouseClicked(e -> {
-                        //TODO
+                        // TODO
                     });
                     Button copySong = new Button("copy song");
                     copySong.setOnMouseClicked(e -> {
-                        //TODO
+                        // TODO
                     });
                     Button deleteSong = new Button("delete song");
                     deleteSong.setOnMouseClicked(e -> {
                         musicFacade.getSongList().removeSong(song);
-                        //TODO ???
+                        // TODO ???
                     });
                     Button publishSong = new Button("publish song");
                     deleteSong.setOnMouseClicked(e -> {
@@ -127,17 +95,66 @@ public class SongListController extends Application {
                 break;
             case 2:
                 songs = musicFacade.openFavorites();
+                for (Song song : songs) {
+                    VBox pane1Content = new VBox(10);
+                    Button playButton = new Button("play song");
+                    playButton.setOnMouseClicked(e -> {
+                        // TODO
+                    });
+                    Button addFav = new Button("add to favorites");
+                    addFav.setOnMouseClicked(e -> {
+                        musicFacade.toggleFavorite(song);
+                    });
+                    Label publishSong = new Label("published");
+                    if (!song.isPublished()) {
+                        publishSong.setText("unpublished");
+                    }
+                    pane1Content.getChildren().addAll(
+                            new Label("Ratings: " + song.getAverageRating()),
+                            playButton,
+                            addFav,
+                            new Label("Difficulty: " + song.getDifficulty()),
+                            new Label("Description \n" + song.getDescription()),
+                            new Label("Genres \n" + song.getGenres()),
+                            publishSong);
+                    TitledPane pane1 = new TitledPane(song.getTitle() + " - " + song.getAuthor().getUsername(),
+                            pane1Content);
+                    accordion.getPanes().add(pane1);
+                }
                 break;
             case 3:
                 songs = musicFacade.openPublicSongs();
+                for (Song song : songs) {
+                    VBox pane1Content = new VBox(10);
+                    Button playButton = new Button("play song");
+                    playButton.setOnMouseClicked(e -> {
+                        // TODO
+                    });
+                    Button addFav = new Button("add to favorites");
+                    addFav.setOnMouseClicked(e -> {
+                        musicFacade.toggleFavorite(song);
+                    });
+                    Label publishSong = new Label("published");
+                    if (!song.isPublished()) {
+                        publishSong.setText("unpublished");
+                    }
+                    pane1Content.getChildren().addAll(
+                            new Label("Ratings: " + song.getAverageRating()),
+                            playButton,
+                            addFav,
+                            new Label("Difficulty: " + song.getDifficulty()),
+                            new Label("Description \n" + song.getDescription()),
+                            new Label("Genres \n" + song.getGenres()),
+                            publishSong);
+                    TitledPane pane1 = new TitledPane(song.getTitle() + " - " + song.getAuthor().getUsername(),
+                            pane1Content);
+                    accordion.getPanes().add(pane1);
+                }
                 break;
-
+                
             default:
                 break;
         }
-    }
-
-    public void songDropdown() {
     }
 
     @Override
