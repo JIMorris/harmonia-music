2da032439b08337fcd3e3a9f6daa6a6681126a54
ekeisler321 <edirisk.21@gmail.com>
2025-04-26 19:15:32 -0400

    Pushing SettingsController.java (needs logout method implemented)

--- src/main/java/com/harmoniamusic/SettingsController.java
+++ src/main/java/com/harmoniamusic/SettingsController.java
@@ -1,11 +1,23 @@
 package com.harmoniamusic;
 
 import java.io.IOException;
-
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 import com.model.MusicFacade;
 
 public class SettingsController {
     private MusicFacade musicFacade = MusicFacade.getInstance();
+
+    @FXML
+    private Button LogoutButton()
+    //TODO
+
 
     private void goToHome() throws IOException {
         App.setBar("homeBar");
