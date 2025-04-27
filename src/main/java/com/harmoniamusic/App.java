1c144b49701be2e0de8437ac4e5d725f1b261374
scartisp <simion.cartis1@gmail.com>
2025-04-27 16:59:58 -0400

    corrected setRoot method in App

--- src/main/java/com/harmoniamusic/App.java
+++ src/main/java/com/harmoniamusic/App.java
@@ -35,11 +35,11 @@ public class App extends Application {
         //  SongEditPlayController.openSong(songs.get(2));
     }
 
-    static FXMLLoader setRoot(String fxml) throws IOException {
+    static void setRoot(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         Parent view = fxmlLoader.load();
         scene.setRoot(view);
-        return fxmlLoader;
+        
     }
     
     static void setRootAndInitialize(String fxml) throws IOException {
